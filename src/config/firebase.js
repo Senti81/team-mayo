8a12c6face6f7c8c910ffd249ce8e99dead66240
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2024-10-21 15:05:40 +0200

    using .env to store secrets

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -11,15 +11,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 }
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBAflJuABT1fHYygtTz1Fht_2WK1YnleZM",
-//   authDomain: "haushalt-app-3271e.firebaseapp.com",
-//   projectId: "haushalt-app-3271e",
-//   storageBucket: "haushalt-app-3271e.appspot.com",
-//   messagingSenderId: "106529297080",
-//   appId: "1:106529297080:web:8f3727e322cfbd81c75a4d"
-// }
-
 const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
