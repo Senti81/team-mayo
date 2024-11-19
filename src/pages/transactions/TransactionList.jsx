6889221321c7d4616ca413b5b051388766aabdae
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2024-11-19 12:06:57 +0100

    added filter function (own / all transactions)

--- src/pages/transactions/TransactionList.jsx
+++ src/pages/transactions/TransactionList.jsx
@@ -11,56 +11,63 @@ const TransactionList = () => {
   const { user } = useAuth()
   const { transactions, fetchTransactions, deleteTransaction, loading, error } = useTransaction()
   const [transaction, setTransaction] = useState(null);
+  const [ownTransactions, setOwnTransactions] = useState(true)
 
   useEffect(() => { fetchTransactions() }, [user])
 
   const currentMonthTransactions = transactions.filter((transaction) => {
     const transactionDate = transaction.createdAt.toDate();
-    const now = new Date();
-    return transactionDate.getMonth() === now.getMonth() && transactionDate.getFullYear() === now.getFullYear();
-  });
+    const now = new Date()
+    return (
+      transactionDate.getMonth() === now.getMonth() &&
+      transactionDate.getFullYear() === now.getFullYear() &&
+      (!ownTransactions || transaction.userId === user.uid)
+    )
+  })
 
   if (error) signOut(auth)
   if (loading) return <Spinner />
+
   return (
     <>
       <div className="container-fluid">
         <div className='container table-responsive'> 
           <h4 className='text-start my-1 display-6'>{new Date().toLocaleString('de-DE', { month: 'long', year: 'numeric' })}</h4>
+
+          <div className="form-check form-switch">
+            <input className="form-check-input" type="checkbox" role="switch" checked={ownTransactions} onChange={(e) => setOwnTransactions(!ownTransactions)}/>
+            <label className="form-check-label" >Zeige nur eigene</label>
+          </div>
+
           <table className='table table-sm'>
             <thead>
               <tr>
                 <th scope="col" className='text-start'>#</th>
                 <th scope="col" className='text-end'>Betrag</th>
-                <th scope="col" className='text-center'>Zeitpunkt</th>
+                <th scope="col" className='text-end'>Zeitpunkt</th>
                 <th scope="col"></th>
               </tr>
             </thead>
             <tbody className='table-group-divider'>
-            {currentMonthTransactions
-              .filter((transaction) => transaction.userId === user.uid)
-              .map((transaction, index) => (
+              {currentMonthTransactions.map((transaction, index) => (
                 <tr key={transaction.id}
-                className='table-primary'>
-                <td className='text-start'>
-                  {index +1}
-                </td>
-                <td className='text-end'>
-                  {transaction.amount.toFixed(2)} €
-                </td>
-                <td className='text-center'>
-                  {transaction.createdAt.toDate().toLocaleString()}
-                </td>
-                <td className='text-end'>
-                  <i
-                    className="bi bi-trash"
-                    data-bs-toggle="modal"
-                    data-bs-target="#confirmDelete"
-                    onClick={() => setTransaction(transaction)}
-                  />                  
-                </td>
-              </tr>
-            ))}
+                  className={transaction.userId === user.uid ? 'table-primary' : 'table-secondary'}
+                >
+                  <td className='text-start'>{index +1}</td>
+                  <td className='text-end'>{transaction.amount.toFixed(2)} €</td>
+                  <td className='text-end'>{transaction.createdAt.toDate().toLocaleString()}</td>
+                  <td className='text-end'>
+                    {transaction.userId === user.uid &&
+                      <i
+                        className="bi bi-trash"
+                        data-bs-toggle="modal"
+                        data-bs-target="#confirmDelete"
+                        onClick={() => setTransaction(transaction)}
+                      />                  
+                    }
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
