68ad7584d4a81d1c0a939cc1e37145f7a263b694
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2024-12-10 15:29:32 +0100

    added Placeholder component

---
+++ src/components/Placeholder.jsx
@@ -0,0 +1,11 @@
+const Placeholder = () => (
+  <p className="placeholder-glow">
+    <span className="placeholder w-100"></span>
+    <span className="placeholder w-75"></span>
+    <span className="placeholder w-50"></span>
+    <span className="placeholder w-75"></span>
+    <span className="placeholder w-100"></span>
+  </p>
+)
+
+export default Placeholder
